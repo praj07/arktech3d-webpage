72639f878b06f6cf9f13c2d0371299c4928dc5c5
monilpatel023 <54343919+monilpatel023@users.noreply.github.com>
2021-02-22 19:04:04 -0500

    Add files via upload

---
+++ arktech3d/src/components/pages/Product.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../../App.css';
+import Footer from '../Footer';
+import ProductDisplay from '../ProductDisplay';
+
+function Product() {
+    return(
+        <>
+            <ProductDisplay />
+            <Footer />
+        </>
+    );   
+}
+
+export default Product;
